3c35fa9ebadac0e32d7278fb3705a8b1e6b92266
Lukeghenco <lukeghenco@gmail.com>
2017-09-04 15:43:03 -0400

    Add styling to input labels

--- src/components/FormInput/index.js
+++ src/components/FormInput/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 
 const FormInput = ({ input, label, type, placeholder, meta }) => 
   <div className="uk-margin">
-    {label && <label htmlFor={input.name}>{label}</label>}
-    <input 
-      className="uk-input"
-      {...input}
-      type={type}
-      placeholder={placeholder} 
-    />
-    {meta.touched && meta.error && 
-      <div className="uk-alert-danger" data-uk-alert>{meta.error}</div>
-    }
+    {label && <label className="uk-form-label" htmlFor={input.name}>{label}</label>}
+    <div className="uk-form-controls">
+      <input 
+        className="uk-input"
+        {...input}
+        type={type}
+        placeholder={placeholder} 
+      />
+      {meta.touched && meta.error && 
+        <div className="uk-alert-danger" data-uk-alert>{meta.error}</div>
+      }
+    </div>
   </div>;
 
 export default FormInput;
